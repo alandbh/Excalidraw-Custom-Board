727400bfc1f5bcc4af8321b8610be1c3e5b585d0
Alan Vasconcelos <alandbh@gmail.com>
2023-02-17 11:09:29 -0300

    Update scene.ts

--- src/pages/api/scene.ts
+++ src/pages/api/scene.ts
@@ -3,19 +3,19 @@ import jsonData from "./poc-claro.json"
 
 // console.log(jsonData);
 
-function getJson(project: string, player: string) {
-  let result;
-  fetch('./scene-retail30-magalu.json')
-    .then((response) => response.json())
-    .then((json) => result = json);
-}
+// function getJson(project: string, player: string) {
+//   let result;
+//   fetch('./scene-retail30-magalu.json')
+//     .then((response) => response.json())
+//     .then((json) => result = json);
+// }
 
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(getJson('retail-30', 'magalu'));
+//   console.log(getJson('retail-30', 'magalu'));
   
   res.status(200).json( jsonData )
 }
