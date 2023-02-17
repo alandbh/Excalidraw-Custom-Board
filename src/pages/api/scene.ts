d8893dfa388dddffb4e9ba502d89d9fad558dba0
Alan Vasconcelos <alandbh@gmail.com>
2023-02-16 23:13:05 -0300

    Updating Scene on load

--- src/pages/api/scene.ts
+++ src/pages/api/scene.ts
@@ -1,13 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import jsonData from "./scene-with-shapes-and-images.json"
+import jsonData from "./poc-claro.json"
 
-console.log(jsonData);
+// console.log(jsonData);
 
+function getJson(project: string, player: string) {
+  let result;
+  fetch('./scene-retail30-magalu.json')
+    .then((response) => response.json())
+    .then((json) => result = json);
+}
 
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  console.log(getJson('retail-30', 'magalu'));
+  
   res.status(200).json( jsonData )
 }
